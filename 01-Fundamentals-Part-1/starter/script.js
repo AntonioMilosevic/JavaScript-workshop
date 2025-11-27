70b146a15d7e28cf659cd01bd3190394ef6aa470
Antonio <antoniomilosevic306@gmail.com>
2025-11-27 18:00:31 +0100

    cleared

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,10 @@
-let js = "amazing";
+/* let js = "amazing";
 
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
+
+
 
 let firstName = "Jonas";
 console.log(firstName);
@@ -19,3 +21,27 @@ console.log(myContinent);
 
 let myPopulation = "Male";
 console.log(myPopulation);
+
+
+true;
+console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
