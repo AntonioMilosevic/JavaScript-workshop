00bde1dca61a4431ea5be01b2a5908f52c773fbe
Antonio <antoniomilosevic306@gmail.com>
2025-12-02 22:31:25 +0100

    if else

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -118,3 +118,11 @@ if (age >= 18) {
   const yearsLeft = 18 - age;
   console.log("Sarah is too young. Wait another ${yearsLeft} years;");
 }
+
+const birthYear = 1991;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
