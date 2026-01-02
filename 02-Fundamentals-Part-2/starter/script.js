6138fb08b093b89708c4a3b789fb7e7795b37e36
Antonio <antoniomilosevic306@gmail.com>
2026-01-02 18:58:03 +0100

    array years

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -122,4 +122,37 @@ const friend3 = "Peter";
 const friends = ["Michael", "Steven", "Peter"];
 console.log(friends);
 
-const years = new Array(1991, 1994, 1996, 1969);
+const y = new Array(1991, 1994, 1996, 1969);
+
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends.length);
+
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmidt", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+
+// exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1991, 1985, 1996, 1998, 2000];
+console.log(calcAge(years));
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[2]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.lenght - 1]),
+];
+console.log(ages);
