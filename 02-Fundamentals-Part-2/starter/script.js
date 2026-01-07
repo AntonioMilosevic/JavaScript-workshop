00bb3be87d490cc79d552cbb044e0378287cac71
Antonio <antoniomilosevic306@gmail.com>
2026-01-07 20:19:05 +0100

    while loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -357,7 +357,7 @@ for (let i = 0; i < jonas.length; i++) {
   if (typeof jonas[i] == "number") break;
   console.log(jonas[i], typeof jonas[i]);
 } */
-
+/*
 const jonas = [
   "Jonas",
   "Schemdtmn",
@@ -377,4 +377,22 @@ for (let exercise = 1; exercise < 4; exercise++) {
   for (let rep = 1; rep < 6; rep++) {
     console.log(`Exercise ${exercise}: Lifting weights repetition ${rep}`);
   }
+} */
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`WHILE:Lifting weights repetition ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end ..");
 }
