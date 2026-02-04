4a0865588c5b6a6a5197bd5dfa5ced71c6390007
Antonio <antoniomilosevic306@gmail.com>
2026-02-04 14:12:31 +0100

    fixing

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -17,17 +17,14 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.message').textContent = 'Correct number';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high';
-    score--;
-    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Too high!';
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too low';
-    score--;
-    document.querySelector('.score').textContent = score;
   }
 });
