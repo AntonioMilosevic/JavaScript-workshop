16bd2ba2cde0408561bcc1f9b256b625eb828923
Antonio <antoniomilosevic306@gmail.com>
2026-02-05 18:26:49 +0100

    fixed highscore

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,8 +27,14 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct number';
     document.querySelector('.number').textContent = secretNumber;
 
-    document.querySelector('body').style.backgroundColor = '#60b347'; //css
-    document.querySelector('number').style.width = '30rem'; // css
+    //css
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(`.highscore`).textContent = highscore;
+    }
 
     // when guess is to high
   } else if (guess > secretNumber) {
@@ -56,11 +63,12 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+
   document.querySelector('.message').textContent = 'Start guessing..';
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
 
   document.querySelector('body').style.backgroundColor = '#222'; //css
-  document.querySelector('number').style.width = '15rem'; // css
+  document.querySelector('.number').style.width = '15rem'; // css
 });
