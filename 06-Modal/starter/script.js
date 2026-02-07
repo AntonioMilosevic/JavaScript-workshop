93f76c82771f3cf5885d95bfe815e253bb559167
Antonio <antoniomilosevic306@gmail.com>
2026-02-07 22:47:02 +0100

    preparing

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,5 +3,8 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('open-modal');
+const btnsOpenModal = document.querySelector('.show-modal');
 console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.lenght; i++)
+  console.log(btnsOpenModal[i].textContent);
