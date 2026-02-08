f7e8e25c8f28b24a61b1cf3873216d556b6fda63
Antonio <antoniomilosevic306@gmail.com>
2026-02-08 13:13:22 +0100

    removing hidden class

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -92,3 +92,4 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222'; //css
   document.querySelector('.number').style.width = '15rem'; // css
 });
+
