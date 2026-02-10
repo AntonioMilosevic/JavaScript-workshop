c483848c74698b723fde53a59d16a1d4b26d0f8e
Antonio <antoniomilosevic306@gmail.com>
2026-02-10 22:40:11 +0100

    adding basic elements

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,8 +3,10 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0'); // # sluzi za id"score--0" , tacka je za klase
 const score1El = document.getElementById('score--1'); // getElementById je brze od querySelector
-
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
