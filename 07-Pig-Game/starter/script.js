4f165c26fa58201a8269d0699b1adc6820da71c1
Antonio <antoniomilosevic306@gmail.com>
2026-02-10 16:29:44 +0100

    reseting the score

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,3 +1,8 @@
 'use strict';
 
-const score0 = document.querySelector('#score--0'); // # sluzi za id"score--0"
+// Selecting elements
+const score0El = document.querySelector('#score--0'); // # sluzi za id"score--0" , tacka je za klase
+const score1El = document.getElementById('score--1'); // getElementById je brze od querySelector
+
+score0El.textContent = 0;
+score1El.textContent = 0;
