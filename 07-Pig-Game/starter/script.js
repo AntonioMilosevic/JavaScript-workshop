3232ae3e9acce6fe9c1fc9372221b474187030ef
Antonio <antoniomilosevic306@gmail.com>
2026-02-10 16:34:32 +0100

    removing the dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,5 +4,9 @@
 const score0El = document.querySelector('#score--0'); // # sluzi za id"score--0" , tacka je za klase
 const score1El = document.getElementById('score--1'); // getElementById je brze od querySelector
 
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
