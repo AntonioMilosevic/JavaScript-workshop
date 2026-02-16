a1b2e83d53bdcc5dec32f735b4bac5b79e446d77
Antonio <antoniomilosevic306@gmail.com>
2026-02-16 22:22:20 +0100

    more examp

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -4,8 +4,13 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    const output = `${firstName},You are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, ${firstName} you are millenial`;
+      console.log(str);
+    }
   }
   printAge();
 
