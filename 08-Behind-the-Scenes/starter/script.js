3d74a5776d098b9b7e74eb19a2b21145cbb47e24
Antonio <antoniomilosevic306@gmail.com>
2026-02-17 12:28:40 +0100

    variable with same name

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -4,12 +4,25 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `${firstName},You are ${age}, born in ${birthYear}`;
+    let output = `${firstName},You are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // creating new variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      // reasigning outer scope's variable
+      output = 'NEW OUTPUT';
+
       const str = `Oh, ${firstName} you are millenial`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      
     }
   }
   printAge();
