96cde1d192aaafda663da74ed5c6e5fc66ce8b4c
Antonio <antoniomilosevic306@gmail.com>
2026-03-15 18:06:01 +0100

    checking

--- 06-Modal/final/script.js
+++ 06-Modal/final/script.js
@@ -1,4 +1,4 @@
- 'use strict';
+'use strict';
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -20,10 +20,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
-
+//
 document.addEventListener('keydown', function (e) {
   // console.log(e.key);
-
+  //
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
