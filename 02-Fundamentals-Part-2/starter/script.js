07a56d2535ef469ece4f14173bda19cb97bd9d95
Antonio <antoniomilosevic306@gmail.com>
2026-04-01 19:38:07 +0200

    more of this

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -625,6 +625,29 @@ const calcAge = function (birthYear) {
   console.log(this);
 };
 calcAge(1991);
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda(calcAge);
+
 //  var firstName = 'Matilda'
 
 // const jonas = {
