10b0a7571b52f631449171548d262d2aad13ff25
Antonio <antoniomilosevic306@gmail.com>
2026-04-28 23:30:52 +0200

    modify

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -216,20 +216,20 @@ const game = {
   },
 };
 
-const [players1, players2] = game.players;
-console.log(players1, players2);
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
 
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
 
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
 
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
 
-team1 < team2 && console.log('Team1 is more likely to win');
+// team1 < team2 && console.log('Team1 is more likely to win');
