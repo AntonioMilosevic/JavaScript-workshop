6000322be68475fb6843b7314b4badce943785a7
Antonio <antoniomilosevic306@gmail.com>
2026-05-21 19:53:43 +0200

    game events

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -319,6 +319,21 @@ console.log(events);
 
 gameEvents.delete(64);
 
+console.log(
+  `An event happened on average every ${90 / gameEvents.size}minutes`,
+);
+
+const time = [...gameEvents.keys()].pop();
+
+console.log(
+  `An event happened on average every ${time / gameEvents.size}minutes`,
+);
+
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`${half}[HALF] ${min}:${event}`);
+}
+
 /*
 // 1.
 const events = [...new Set(gameEvents.values())];
