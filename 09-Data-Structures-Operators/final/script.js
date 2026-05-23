ed0c31881fc2d4cdb786d39f52232fabd163c1ef
Antonio <antoniomilosevic306@gmail.com>
2026-05-23 12:53:46 +0200

    str passengers

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -353,6 +353,30 @@ console.log(airline.slice(4));
 console.log(airline.slice(4, 7));
 
 console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log(`You've got middle seat`);
+  else console.log('You got lucky');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+const passenger = 'jOnAS';
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
 
 /*
 // 1.
