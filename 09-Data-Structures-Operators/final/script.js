45b3c4eaebf5264d8618c472260bfe4e08af468f
Antonio <antoniomilosevic306@gmail.com>
2026-05-24 21:05:03 +0200

    ad

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -352,18 +352,18 @@ console.log(airline.indexOf('Portugal'));
 console.log(airline.slice(4));
 console.log(airline.slice(4, 7));
 
-console.log(airline.slice(0, airline.indexOf(' ')));
-console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
 
-console.log(airline.slice(-2));
-console.log(airline.slice(1, -1));
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
 
-const checkMiddleSeat = function (seat) {
-  // B and E are middle seats
-  const s = seat.slice(-1);
-  if (s === 'B' || s === 'E') console.log(`You've got middle seat`);
-  else console.log('You got lucky');
-};
+// const checkMiddleSeat = function (seat) {
+//   // B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') console.log(`You've got middle seat`);
+//   else console.log('You got lucky');
+// };
 
 checkMiddleSeat('11B');
 checkMiddleSeat('23C');
