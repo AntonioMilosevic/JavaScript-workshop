adca9da4e17629aab6caf3718cafbecf7c347944
Antonio <antoniomilosevic306@gmail.com>
2026-06-28 16:35:16 +0200

    string metods

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -55,6 +55,8 @@ const restaurant = {
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+console
+
 // 🔴 Delayed Departure from FAO to TXL (11h25)
 //              Arrival from BRU to FAO (11h45)
 //   🔴 Delayed Arrival from HEL to FAO (12h05)
@@ -459,20 +461,35 @@ console.log(message2.repeat(5));
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
-console.log(text);
-
 document.querySelector('button').addEventListener('click', function () {
   const text = document.querySelector('textarea').value;
   const rows = text.split('\n');
   console.log(rows);
+  console.log(text);
 
-  for (const row in rows) {
+  for (const row of rows) {
     const [first, second] = row.toLowerCase().trim().split('_');
-    const output = `${first}${second.replace(second[0], second[0].toUpperCase)}`;
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
 
-    console.log(output);
+    console.log(output.padEnd(20, ' '));
   }
 });
+
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} ${from} ${to} (${time.replace(':', 'h')})`;
+  console.log(output);
+}
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
 
 // const planesInLine = function(n) {
 //   console.log()
