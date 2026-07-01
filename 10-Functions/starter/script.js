252de37541a0a0f20b780dcacbd4f6c1392ce07b
Antonio <antoniomilosevic306@gmail.com>
2026-07-01 21:30:23 +0200

    more f

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,58 +1,66 @@
 'use strict';
 
-const bookings = [];
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers,
-) {
-  // es5
-  //   numPassengers = numPassengers || 1;
-  //   price = price || 199;
+// const bookings = [];
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers,
+// ) {
+//   // es5
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', '2', '800');
+// createBooking('LH123', 2);
+// createBooking('LH123', undefined, 1000);
+
+// const flight = 'LH234';
+
+// const jonas = {
+//   name: 'Jonas Schmedtman',
+//   passport: 42425236,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 42425236) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // Is the same as doing
+// const flightNum = flight;
+// const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
 
-createBooking('LH123');
-createBooking('LH123', '2', '800');
-createBooking('LH123', 2);
-createBooking('LH123', undefined, 1000);
-
-const flight = 'LH234';
-
-const jonas = {
-  name: 'Jonas Schmedtman',
-  passport: 42425236,
-};
-
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
-
-  if (passenger.passport === 42425236) {
-    alert('Checked in');
-  } else {
-    alert('Wrong passport!');
-  }
-};
-
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
-
-// Is the same as doing
-const flightNum = flight;
-const passenger = jonas;
-
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000000);
-};
-
-newPassport(jonas);
-checkIn(flight, jonas);
+const upperFirstWord = function(str){
+  return str.s
+}
