867f58418f582f32d1933d3791a3ccb027010f3a
Antonio <antoniomilosevic306@gmail.com>
2026-07-19 19:01:54 +0200

    bind

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -179,3 +179,23 @@ lufthansa.buyPlane = function () {
 document
   .querySelector('.buy')
   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVat = value => value + value
+
+console.log(addVAT(100));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addTax2 = addTaxRate(0.23);
+console.log(addTax2(100));
+console.log(addTax2(23));
