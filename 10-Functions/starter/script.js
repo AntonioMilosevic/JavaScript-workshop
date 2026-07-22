73a6ba30d5a58401575ca470099bdafbc8da382d
Antonio <antoniomilosevic306@gmail.com>
2026-07-22 21:26:46 +0200

    ch

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -199,3 +199,18 @@ const addTaxRate = function (rate) {
 const addTax2 = addTaxRate(0.23);
 console.log(addTax2(100));
 console.log(addTax2(23));
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`,
+      ),
+    );
+  },
+};
