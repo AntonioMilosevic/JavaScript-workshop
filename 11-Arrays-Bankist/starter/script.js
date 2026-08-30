c9063cb3c535db903e2aa56c7c66a8f3c109b3ce
Antonio <antoniomilosevic306@gmail.com>
2026-08-30 20:01:41 +0200

    at method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -77,7 +77,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
-//SLice method
+//SLice method, ne utice na originalni arr
 console.log(arr.slice(2));
 console.log(arr.slice(2, 4));
 console.log(arr.slice(-2));
@@ -85,3 +85,34 @@ console.log(arr.slice(-1));
 console.log(arr.slice(1, -2));
 console.log(arr.slice());
 console.log(arr.slice([...arr]));
+
+//SPlice method , utice na originalni arr
+
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// Reverse utice na arr
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// Concat ne mijenja originalne arr
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// Join
+console.log(letters.join(' -  '));
+
+// new at method
+const arrX = [23, 4, 17];
+console.log(arrX[0]);
+console.log(arrX.at(0));
+
+console.log(arrX[arrX.length - 1]);
+console.log(arrX.slice(-1)[0]);
+console.log(arrX.at(-2));
