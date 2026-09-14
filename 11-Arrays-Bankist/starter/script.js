d2a38b2db9a3c0a8dee1967d834ede45c5dbadec
Antonio <antoniomilosevic306@gmail.com>
2026-09-14 13:37:16 +0200

    forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -116,3 +116,25 @@ console.log(arrX.at(0));
 console.log(arrX[arrX.length - 1]);
 console.log(arrX.slice(-1)[0]);
 console.log(arrX.at(-2));
+
+console.log('jonas'.at(1));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${movement}`);
+  }
+}
+
+console.log('---FOREACH---');
+
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${movement}`);
+  }
+});
