7c89549c861add53249a931e5e4f07570bb37053
Antonio <antoniomilosevic306@gmail.com>
2026-09-16 20:50:59 +0200

    map for each

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -121,20 +115,31 @@ console.log('jonas'.at(1));
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-for (const movement of movements) {
+//for (const movement of movements)
+for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1}:You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${movement}`);
+    console.log(`Movement ${i + 1}:You withdrew ${Math.abs(movement)}`);
   }
 }
 
 console.log('---FOREACH---');
 
-movements.forEach(function (movement) {
-  if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}:You deposited ${mov}`);
   } else {
-    console.log(`You withdrew ${movement}`);
+    console.log(`Movement ${i + 1}:You withdrew ${Math.abs(mov)}`);
   }
+});
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}:${value}`);
 });
